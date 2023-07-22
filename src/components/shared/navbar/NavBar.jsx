571ad8eca6925a8e9f4d08f2f0bb941243e345f3
rh-abir <rhabir.co@gmail.com>
2023-07-22 11:31:42 +0600

    make navBar

--- src/components/shared/navbar/NavBar.jsx
+++ src/components/shared/navbar/NavBar.jsx
@@ -1,11 +1,90 @@
-
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
-    return (
-        <div>
-            This is Nav Bar
+  const navLink = (
+    <>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/Colleges">Colleges</Link>
+      </li>
+      <li>
+        <Link to="/admissionColleges">Admission</Link>
+      </li>
+      <li>
+        <Link to="/my-colleges">My College</Link>
+      </li>
+    </>
+  );
+
+  const isShow = false;
+
+  return (
+    <div className="navbar bg-base-100 container mx-auto">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {navLink}
+
+          </ul>
         </div>
-    );
+        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          {navLink}
+        </ul>
+      </div>
+      <div className="navbar-end">
+       { ! isShow ?  <button className="btn">Log In</button> :
+
+          <div className="">
+            <div className="dropdown dropdown-end">
+              <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                <div className="w-10 rounded-full">
+                  <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                </div>
+              </label>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content mt-2 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+              >
+                <li>
+                  <a>
+                    Profile
+                  </a>
+                </li>
+                <li>
+                  <a>Logout</a>
+                </li>
+              </ul>
+            </div>
+          </div>}
+
+
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
